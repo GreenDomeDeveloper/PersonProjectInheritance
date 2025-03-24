bfb53de3550d468b6e05452ad7bbdc40e11ae23a
GreenDomeDeveloper <will@fcctulsa.org>
2025-03-24 15:31:24 +0000

    ArrayList Demo Added

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Person one = new Person();
         Person two = new Person("Mary Jones", 17);
         Student three = new Student();
         Student four = new Student("Carl Smith",42,3.5);
+        ArrayList<Person> village = new ArrayList<Person>();
         System.out.println("Welcome to the Person Project");
 
         System.out.println("Testing Persons");
@@ -22,5 +25,16 @@ public class Main {
         System.out.println("\nFinal testing");
         Person five = new Student("Kim Turner",50,4.0);
         System.out.println(five);
+
+        System.out.println("\nTesting ArrayList");
+        village.add(one);
+        village.add(two);
+        village.add(three);
+        village.add(four);
+        village.add(five);
+
+        for (int i=0; i<village.size(); i++){
+            System.out.println("Villager #"+(i+1)+": "+village.get(i)+"\n");
+        }
     }
 }
